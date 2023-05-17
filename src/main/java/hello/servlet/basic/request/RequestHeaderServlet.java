f26a7644ab613d9aaa928e393552d1b068b13532
icoo08217 <icoo08216@gmail.com>
2023-05-17 23:21:17 +0900

    HTTP 요청 데이터 - GET 쿼리 파라미터

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -86,6 +86,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println();
     }
 
+    // 기타 정보
     private void printEtc(HttpServletRequest request) { System.out.println("--- 기타 조회 start ---");
         System.out.println("[Remote 정보]");
         System.out.println("request.getRemoteHost() = " + request.getRemoteHost()); //
