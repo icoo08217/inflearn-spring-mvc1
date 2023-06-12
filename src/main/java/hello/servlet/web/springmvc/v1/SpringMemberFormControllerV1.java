5adb6ab92dca25a6009fe7db97c5efe26651c02f
icoo08217 <icoo08216@gmail.com>
2023-06-12 19:18:11 +0900

    스프링 MVC - 도입 시작

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // component 스캔의 대상이 되는 annotation
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
